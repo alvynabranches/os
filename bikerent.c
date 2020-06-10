b4b0f98580b2ec5e52021f3bcc906a549818646e
Alvyn Abranches <alvynabranches@gmail.com>
2020-06-10 19:36:20 +0530

    10062020v6

--- bikerent.c
+++ bikerent.c
@@ -67,128 +67,155 @@ int input()
 // 	printf("\n");
 // }
 
-void bikeMenu(FILE *fp){
-	int choice;
-	do {
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
-		printf("\t\t\tBike Rental System\n"); 
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// void bikeMenu(FILE *fp){
+// 	int choice;
+// 	do {
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// 		printf("\t\t\tBike Rental System\n"); 
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
 	
-		printf("Press 1 : Add Bike\n"); 
-		printf("Press 2 : Update Bike\n"); 
-		printf("Press 3 : Delete Bike\n"); 
-		printf("Press 4 : Back\n");
-		printf("Press 0 : Quit\n");
-		printf("Enter your choice:\n"); 
+// 		printf("Press 1 : Add Bike\n"); 
+// 		printf("Press 2 : Update Bike\n"); 
+// 		printf("Press 3 : Delete Bike\n"); 
+// 		printf("Press 4 : Back\n");
+// 		printf("Press 0 : Quit\n");
+// 		printf("Enter your choice:\n"); 
 
-		choice = input(); 
+// 		choice = input(); 
 		  
-		switch (choice) { 
-		case 1: { 
-			system("clear");
-		    printf("Add Bike\n");
-			writeData(fp); 
-		    break; 
-		} 
-		case 2: { 
-			system("clear");
-		    printf("Update Bike\n");
-		    break; 
-		} 
-		case 3: { 
-			system("clear");
-		    printf("Delete Bike\n"); 
-		    break; 
-		} 
-		case 4: { 
-			system("clear");
-		    printf("Main Menu\n");
-			mainMenu(fp); 
-		    break; 
-		} 
-		case 0: { 
-			system("clear");
-		    printf("Quiting.....\n");
-		    break;
-		}
-		default: 
-			system("clear");
-		    printf("wrong Input\n"); 
-		}
-	} while(choice !=0);
+// 		switch (choice) { 
+// 		case 1: { 
+// 			system("clear");
+// 		    printf("Add Bike\n");
+// 			writeData(fp); 
+// 		    break; 
+// 		} 
+// 		case 2: { 
+// 			system("clear");
+// 		    printf("Update Bike\n");
+// 		    break; 
+// 		} 
+// 		case 3: { 
+// 			system("clear");
+// 		    printf("Delete Bike\n"); 
+// 		    break; 
+// 		} 
+// 		case 4: { 
+// 			system("clear");
+// 		    printf("Main Menu\n");
+// 			mainMenu(fp); 
+// 		    break; 
+// 		} 
+// 		case 0: { 
+// 			system("clear");
+// 		    printf("Quiting.....\n");
+// 		    break;
+// 		}
+// 		default: 
+// 			system("clear");
+// 		    printf("wrong Input\n"); 
+// 		}
+// 	} while(choice !=0);
 
-}
+// }
 
-void mainMenu(FILE *fp){
-	int choice;
-	do{
+// void mainMenu(FILE *fp){
+// 	int choice;
+// 	do{
 
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
-		printf("\t\t\tBike Rental System\n"); 
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
-		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// 		printf("\t\t\tBike Rental System\n"); 
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
+// 		printf("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"); 
 	
-		printf("Press 1 : Bike Details\n"); 
-		printf("Press 2 : Display Bike Details\n"); 
-		printf("Press 3 : Rent Bike\n"); 
-		printf("Press 4 : Bill\n"); 
-		printf("Press 5 : Current Status of Bike\n"); 
-		printf("Press 0 : Quit\n");
-		printf("Enter your choice:\n"); 
+// 		printf("Press 1 : Bike Details\n"); 
+// 		printf("Press 2 : Display Bike Details\n"); 
+// 		printf("Press 3 : Rent Bike\n"); 
+// 		printf("Press 4 : Bill\n"); 
+// 		printf("Press 5 : Current Status of Bike\n"); 
+// 		printf("Press 0 : Quit\n");
+// 		printf("Enter your choice:\n"); 
 
-		choice = input(); 
+// 		choice = input(); 
 		  
-		switch (choice) { 
-		case 1: { 
-			system("clear");
-		    bikeMenu(fp);
-		    break; 
-		} 
-		case 2: { 
-			system("clear");
-		    printf("Display Bike Details\n");
-			readData(fp); 
-		    break; 
-		} 
-		case 3: { 
-			system("clear");
-		    printf("Rent Bike\n"); 
-		    break; 
-		} 
-		case 4: { 
-			system("clear");
-		    printf("Bill Bike\n"); 
-		    break; 
-		}
-		case 5: { 
-			system("clear");
-		    printf("Current Status of Bike\n"); 
-		    break; 
-		}
-		case 0: { 
-			system("clear");
-		    printf("Quiting.....\n");
-		    break;
-		}
-		default: 
-			system("clear");
-		    printf("wrong Input\n"); 
-		}
-	} while(choice !=0);
+// 		switch (choice) { 
+// 		case 1: { 
+// 			system("clear");
+// 		    bikeMenu(fp);
+// 		    break; 
+// 		} 
+// 		case 2: { 
+// 			system("clear");
+// 		    printf("Display Bike Details\n");
+// 			readData(fp); 
+// 		    break; 
+// 		} 
+// 		case 3: { 
+// 			system("clear");
+// 		    printf("Rent Bike\n"); 
+// 		    break; 
+// 		} 
+// 		case 4: { 
+// 			system("clear");
+// 		    printf("Bill Bike\n"); 
+// 		    break; 
+// 		}
+// 		case 5: { 
+// 			system("clear");
+// 		    printf("Current Status of Bike\n"); 
+// 		    break; 
+// 		}
+// 		case 0: { 
+// 			system("clear");
+// 		    printf("Quiting.....\n");
+// 		    break;
+// 		}
+// 		default: 
+// 			system("clear");
+// 		    printf("wrong Input\n"); 
+// 		}
+// 	} while(choice !=0);
 
-}
+// }
 
-int * date(int date, int month, int year){
-	int d[3] ={date, month, year};
+int* date(int date, int month, int year){
+	static int d[3];
+	d[0] = date; d[1] = month; d[2] = year;
 	return d;
 }
 
-void expiry_alert(int * expiry, int * current)
+int expiry_alert(int* expiry, int* current)
 {
-	unsigned short int expired = 0;
+	/* 
+	// 0 -> not expired; 
+	// 1 -> expired; 
+	// 2 -> expiring today; 
+	// 3 -> does not match any situation; && this situation will never arraise unless the code is wrong;
+	*/
+	if (expiry[2] < current[2])
+		return 1;
+	else if (expiry[2] > current[2])
+		return 0;
+	else if (expiry[2] == current[2])
+	{
+		if (expiry[1] < current[1])
+			return 1;
+		else if (expiry[1] > current[1])
+			return 0;
+		else if (expiry[1] == current[1])
+		{
+			if (expiry[0] < current[0])
+				return 1;
+			else if (expiry[0] > current[0])
+				return 0;
+			else if (expiry[0] == current[0])
+				return 2;
+		}
+	}
+	return 3;
 }
 
 void main() 
@@ -206,6 +233,15 @@ void main()
 	// 	mainMenu(fp);
 	// }
 	// fclose(fp);
+	int expiry[] = {10, 6, 2020};
+	int current[] = {10, 6, 2020};
+	int alert = expiry_alert(expiry, current);
+	if (alert == 1)
+		printf("Expired. Please renew immadietely, otherwise you will have to pay a fine.\n");
+	else if (alert == 2)
+		printf("Expiring Today. Please renew it as soon as possible.\n");
+	else if (alert == 0)
+		printf("Dont worry. You have enough of time.\n");
 }
 
 
